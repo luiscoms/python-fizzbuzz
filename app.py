67f918f9f8bd9bb3057b9cbf59b3e0921025da1d
Luis Fernando Gomes <dev@luiscoms.com.br>
2017-06-30 00:02:55 -0300

    Change pre color

--- app.py
+++ app.py
@@ -11,14 +11,29 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>FizzBuzz</h1>
-    <p>It is currently {time}.</p>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+        pre {
+            background: #CCCCCC
+        }
+        </style>
+        <title>FizzBuzz</title>
+    </head>
+    <body>
+        <h1>FizzBuzz</h1>
+        <p>It is currently {time}.</p>
 
-    <pre>
+        <pre>
 {result}
-    </pre>
+        </pre>
+    </body>
+    </html>
     """.format(time=the_time,
-               result="\n".join([fizzbuzz(i) for i in range(1, 101)]))
+               # result="\n".join([fizzbuzz(i) for i in range(1, 101)]),
+               result="\n".join(list(map(fizzbuzz, range(1, 101))))
+               )
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
